cc0d060d8c7ca7c234c7aca8f159024502be1287
cuongne <45033626+cuongne@users.noreply.github.com>
2020-04-06 14:34:09 +0700

    Update express coderX

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const express = require("express");
 const app = express();
+
+var userRoute = require('./routes/user.route');
+
 const port = 3000;
-app.get('/', (req, res) => res.send('Hello asd!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+var db = require('./db');
+
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('view engine', 'pug');
+app.get('/', function(req, res) {
+    res.render('index')
+});
+
+app.use(express.static('public'));
+app.use('/users', userRoute);
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
